1a1412563a069cfabc5fba85a582fbb1bdd37022
Michael C. Tiberio <github@tiberio.org>
2022-10-31 11:50:10 -0700

    Render an empty scene as all black

---
+++ src/RayTracerLib/RayTracer.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace RayTracer;
+public class RayTracer
+{
+    public Bitmap Render()
+    {
+        return GetEmptyScene(1024, 1024);
+    }
+
+    private Bitmap GetEmptyScene(int width, int height)
+    {
+        Bitmap bitmap = new (width, height);
+
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < width; y++)
+                bitmap.SetPixel(x, y, Color.Black);
+
+        return bitmap;
+    }
+}
--- test/RayTracer.Tests/RayTracerLibTests.cs
+++ test/RayTracer.Tests/RayTracerLibTests.cs
@@ -1,11 +1,32 @@
 using Xunit;
+using System.Drawing;
 
 namespace RayTracer.Tests;
 
-public class UnitTest1
+public class RayTracerLibTests
 {
     [Fact]
-    public void Test1()
+    public void EmptySceneShouldBeBlack()
     {
+        const int width = 1024;
+        const int height = 1024;
+
+        RayTracer rayTracer = new ();
+
+        Bitmap bitmap = rayTracer.Render();
+
+        Assert.Equal(width, bitmap.Width);
+        Assert.Equal(height, bitmap.Height);
+
+        var expectedColorArgb = Color.Black.ToArgb();
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var actualColor = bitmap.GetPixel(x, y);
+                Assert.Equal(expectedColorArgb, actualColor.ToArgb());
+            }
+        }
     }
 }
