ad5645eaffc4fef93cc44e4d581925f08a16379d
Michael C. Tiberio <github@tiberio.org>
2022-11-04 11:15:13 -0700

    Adding Nearest(Vector point) to Ray

--- src/RayTracerLib/Ray.cs
+++ src/RayTracerLib/Ray.cs
@@ -8,6 +8,9 @@ internal struct Ray
     public Ray(Vector location, Vector direction) =>
         (Position, Direction) = (location, direction.Unit());
 
+    public Vector Nearest(Vector point) =>
+        (Direction * (point - Position)) * Direction + Position;
+
     public static Ray XAxis { get; } = new Ray(Vector.Origin, Vector.XBasis);
     public static Ray YAxis { get; } = new Ray(Vector.Origin, Vector.YBasis);
     public static Ray ZAxis { get; } = new Ray(Vector.Origin, Vector.ZBasis);
--- test/RayTracer.Tests/RayTests.cs
+++ test/RayTracer.Tests/RayTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace RayTracer.Tests;
@@ -25,6 +26,41 @@ public sealed class RayTests
     {
         Ray expected = new Ray(Vector.Origin, Vector.ZBasis);
         Ray actual = Ray.ZAxis;
+        MathAssert.Equal(expected, actual);
+    }
+
+    internal static IEnumerable<object[]> NearestShouldFindPointOnRayNearestToGivenPointData()
+    {
+        yield return new object []
+            { new Ray(Vector.Origin, Vector.XBasis), new Vector(3.0, 4.0, 5.0), new Vector(3.0, 0.0, 0.0) };
+
+        yield return new object []
+            { new Ray(Vector.Origin, Vector.YBasis), new Vector(3.0, 4.0, 5.0), new Vector(0.0, 4.0, 0.0) };
+
+        yield return new object []
+            { new Ray(Vector.Origin, Vector.ZBasis), new Vector(3.0, 4.0, 5.0), new Vector(0.0, 0.0, 5.0) };
+
+        yield return new object []
+            { new Ray(Vector.Origin, new Vector(2.0, 3.0, 6.0)), new Vector(147.0, 196.0, 245.0), new Vector(96.0, 144.0, 288.0) };
+
+        yield return new object []
+            { new Ray(new Vector(10.0, 12.0, 14.0), Vector.XBasis), new Vector(3.0, 4.0, 5.0), new Vector(3.0, 12.0, 14.0) };
+
+        yield return new object []
+            { new Ray(new Vector(10.0, 12.0, 14.0), Vector.YBasis), new Vector(3.0, 4.0, 5.0), new Vector(10.0, 4.0, 14.0) };
+
+        yield return new object []
+            { new Ray(new Vector(10.0, 12.0, 14.0), Vector.ZBasis), new Vector(3.0, 4.0, 5.0), new Vector(10.0, 12.0, 5.0) };
+
+        yield return new object []
+            { new Ray(new Vector(10.0, 12.0, 14.0), new Vector(2.0, 3.0, 6.0)), new Vector(157.0, 208.0, 259.0), new Vector(106.0, 156.0, 302.0) };
+    }
+
+    [Theory]
+    [MemberData(nameof(NearestShouldFindPointOnRayNearestToGivenPointData))]
+    internal void NearestShouldFindPointOnRayNearestToGivenPoint(Ray ray, Vector point, Vector expected)
+    {
+        Vector actual = ray.Nearest(point);
         MathAssert.Equal(expected, actual);
     }
 }
