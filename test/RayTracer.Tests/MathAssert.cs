0b59d9ad05c0db7fe7f9b67f29a70ff86898d470
Michael C. Tiberio <github@tiberio.org>
2022-11-04 11:13:24 -0700

    Using IEqualityComparer<> objects in MathAssert

--- test/RayTracer.Tests/MathAssert.cs
+++ test/RayTracer.Tests/MathAssert.cs
@@ -1,24 +1,49 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace RayTracer.Tests;
 
 internal static class MathAssert
 {
-    private const int precision = 15;
-
     public static void Equal(double expected, double actual) =>
         Assert.Equal(expected, actual, precision);
 
-    public static void Equal(Vector expected, Vector actual)
+    public static void Equal(Vector expected, Vector actual) =>
+        Assert.Equal(expected, actual, vectorEqualityComparer);
+
+    public static void Equal(Ray expected, Ray actual) =>
+        Assert.Equal(expected, actual, rayEqualityComparer);
+
+    private static readonly VectorEqualityComparer vectorEqualityComparer = new ();
+    private static readonly RayEqualityComparer rayEqualityComparer = new ();
+
+    private const int precision = 14;
+
+    private sealed class VectorEqualityComparer : IEqualityComparer<Vector>
     {
-        Assert.Equal(expected.X, actual.X, precision);
-        Assert.Equal(expected.Y, actual.Y, precision);
-        Assert.Equal(expected.Z, actual.Z, precision);
+        public bool Equals(Vector lhs, Vector rhs) =>
+            Vector.Equals(
+                new Vector(
+                    Math.Round(lhs.X, precision),
+                    Math.Round(lhs.Y, precision),
+                    Math.Round(lhs.Z, precision)),
+                new Vector(
+                    Math.Round(rhs.X, precision),
+                    Math.Round(rhs.Y, precision),
+                    Math.Round(rhs.Z, precision)));
+
+        public int GetHashCode(Vector _) =>
+            throw new NotImplementedException();
     }
 
-    public static void Equal(Ray expected, Ray actual)
+    private sealed class RayEqualityComparer : IEqualityComparer<Ray>
     {
-        MathAssert.Equal(expected.Position, actual.Position);
-        MathAssert.Equal(expected.Direction, actual.Direction);
+        public bool Equals(Ray lhs, Ray rhs) =>
+            vectorEqualityComparer.Equals(lhs.Position, rhs.Position) &&
+            vectorEqualityComparer.Equals(lhs.Direction, rhs.Direction);
+
+        public int GetHashCode(Ray _) =>
+            throw new NotImplementedException();
     }
 }
