8ad99ae188f777891b5df14d149266273dfaf147
Michael C. Tiberio <github@tiberio.org>
2022-11-05 10:38:43 -0700

    Refactor SquarePixelTestData()

--- test/RayTracer.Tests/PixelCenterCameraTests.cs
+++ test/RayTracer.Tests/PixelCenterCameraTests.cs
@@ -25,36 +25,52 @@ public sealed class PixelCenterCameraTests
     private static IEnumerable<object[]> SquarePixelTestData() =>
         new object[][]
         {
-            new object [] { 1, 1, expectedSquare1By1 },
-            new object [] { 1, 2, expectedSquare1By2 },
-            new object [] { 2, 1, expectedSquare2By1 },
-            new object [] { 2, 2, expectedSquare2By2 },
+            expectedSquare1By1,
+            expectedSquare1By2,
+            expectedSquare2By1,
+            expectedSquare2By2,
         };
 
     private static Vector focus = new (0.0, 0.0, 1.0);
 
-    private static IEnumerable<Ray> expectedSquare1By1 = new Ray []
+    private static object[] expectedSquare1By1 =
     {
-        new Ray(focus, -Vector.ZBasis)
+        1, 1,      // rows, columns
+        new Ray[]  // expected
+        {
+            new Ray(focus, -Vector.ZBasis)
+        }
     };
 
-    private static IEnumerable<Ray> expectedSquare1By2 = new Ray []
+    private static object[] expectedSquare1By2 =
     {
-        new Ray(focus, new Vector(-0.05, 0.0, -1.0)),
-        new Ray(focus, new Vector( 0.05, 0.0, -1.0))
+        1, 2,      // rows, columns
+        new Ray[]  // expected
+        {
+            new Ray(focus, new Vector(-0.05, 0.0, -1.0)),
+            new Ray(focus, new Vector( 0.05, 0.0, -1.0))
+        }
     };
 
-    private static IEnumerable<Ray> expectedSquare2By1 = new Ray []
+    private static object[] expectedSquare2By1 =
     {
-        new Ray(focus, new Vector(0.0,  0.05, -1.0)),
-        new Ray(focus, new Vector(0.0, -0.05, -1.0))
+        2, 1,      // rows, columns
+        new Ray[]  // expected
+        {
+            new Ray(focus, new Vector(0.0,  0.05, -1.0)),
+            new Ray(focus, new Vector(0.0, -0.05, -1.0))
+        }
     };
 
-    private static IEnumerable<Ray> expectedSquare2By2 = new Ray []
+    private static object[] expectedSquare2By2 =
     {
-        new Ray(focus, new Vector(-0.05,  0.05, -1.0)),
-        new Ray(focus, new Vector( 0.05,  0.05, -1.0)),
-        new Ray(focus, new Vector(-0.05, -0.05, -1.0)),
-        new Ray(focus, new Vector( 0.05, -0.05, -1.0))
+        2, 2,      // rows, columns
+        new Ray[]  // expected
+        {
+            new Ray(focus, new Vector(-0.05,  0.05, -1.0)),
+            new Ray(focus, new Vector( 0.05,  0.05, -1.0)),
+            new Ray(focus, new Vector(-0.05, -0.05, -1.0)),
+            new Ray(focus, new Vector( 0.05, -0.05, -1.0))
+        }
     };
 }
